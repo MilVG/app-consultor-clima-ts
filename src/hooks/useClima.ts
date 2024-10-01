d6d3310602a198fc2cf23eefd1f0c4eb112d22b7
notlim_dev <elmotigre6@gmail.com>
2024-10-01 02:44:08 -0500

    0030: Solución problema de protocolo http a https
    se modificó la la url de http a https.

--- src/hooks/useClima.ts
+++ src/hooks/useClima.ts
@@ -62,7 +62,7 @@ export default function useClima() {
     setClima(initialState)
     try {
       const appId = import.meta.env.VITE_API_KEY
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.ciudad},${search.pais}&appid=${appId}`
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.ciudad},${search.pais}&appid=${appId}`
 
 
       const { data } = await axios(geoUrl)
