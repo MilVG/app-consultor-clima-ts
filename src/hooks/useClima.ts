008898eead56dc5b7671b142f842a699955f0179
notlim_dev <elmotigre6@gmail.com>
2024-09-30 22:38:06 -0500

    0013: Variables de Entorno
    Se creó la variable para manejar el keyid para manejar la api,
    definiendo con la nomeclatura de vite anteponiendo VITE para su
    creación.

--- src/hooks/useClima.ts
+++ src/hooks/useClima.ts
@@ -5,7 +5,7 @@ export default function useClima() {
 
   const fetchClima = async (search: SearchType) => {
     try {
-      const appId = 'c94ae2dfa7ec61116279a2f87775aa28'
+      const appId = import.meta.env.VITE_API_KEY
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.pais},${search.pais}&appid=${appId}`
 
       const { data } = await axios(geoUrl)
