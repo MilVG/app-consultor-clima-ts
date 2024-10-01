10f417e93b94ee3e072abc831e104808b5a0f9d6
notlim_dev <elmotigre6@gmail.com>
2024-10-01 02:04:49 -0500

    0024: Estilos Para el Componente ClimaDetalle
    Se Estilizó los elementos de muestra de datos.

--- src/components/ClimaDetalle/ClimaDetalle.tsx
+++ src/components/ClimaDetalle/ClimaDetalle.tsx
@@ -1,6 +1,6 @@
 import { Clima } from "../../hooks/useClima"
 import { formatTemperature } from "../../utils"
-
+import styles from "./ClimaDetalle.module.css"
 
 
 type ClimaProps = {
@@ -8,10 +8,10 @@ type ClimaProps = {
 }
 export default function ClimaDetalle({ clima }: ClimaProps) {
   return (
-    <div>
+    <div className={styles.container}>
       <h2> Clima de: {clima.name}</h2>
-      <p>{formatTemperature(clima.main.temp)}&deg;C</p>
-      <div>
+      <p className={styles.current}>{formatTemperature(clima.main.temp)}&deg;C</p>
+      <div className={styles.temperatures}>
         <p>Min: <span>{formatTemperature(clima.main.temp_min)}&deg;C</span></p>
         <p>Max: <span>{formatTemperature(clima.main.temp_max)}&deg;C</span></p>
       </div>
