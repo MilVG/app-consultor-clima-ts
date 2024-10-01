5d48899acaf692b594e9fde678e1e89646679e92
notlim_dev <elmotigre6@gmail.com>
2024-10-01 02:28:24 -0500

    0027: Incorporación de Spinner
    Se agregó el snipper al la estructura general de App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import styles from './App.module.css'
 import ClimaDetalle from './components/ClimaDetalle/ClimaDetalle'
 import Form from './components/Form/Form'
+import Spinner from './components/Spinner/Spinner'
 import useClima from './hooks/useClima'
 function App() {
 
-  const { fetchClima, clima, datosvaciosClima } = useClima()
+  const { fetchClima, clima, datosvaciosClima, loading } = useClima()
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
@@ -12,6 +13,7 @@ function App() {
         <Form
           fetchClima={fetchClima}
         />
+        {loading && <Spinner />}
         {datosvaciosClima &&
 
           <ClimaDetalle
